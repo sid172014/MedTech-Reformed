256bf267432146015b0a47ac8e54180f4f524272
Deepak <deepakdas538@gmail.com>
2024-04-08 19:06:32 +0530

    stylelab

--- frontend/src/components/main/Sections/pages/Lab.jsx
+++ frontend/src/components/main/Sections/pages/Lab.jsx
@@ -4,30 +4,51 @@ import axios from 'axios';
 import './lab.css'
 
 const Lab = () => {
-    
-    const [data,setData] = useState([]);
+
+    const [data, setData] = useState([]);
 
 
     useEffect(() => {
         const getData = async () => {
-            const response = await axios.get('http://localhost:3000/users/mydetails');  
+            const response = await axios.get('http://localhost:3000/users/mydetails');
             setData(() => {
                 return response.data.medInfo;
             });
         };
         getData();
-    },[]);
+    }, []);
 
-    return(
-        <div>
-            {data.length ===0 ? null : data.map((item) => {
-                return <div key={item._id}>
-                    <div>{item.tests.map((element) => {return element})}</div>
-                    <div>{new Date(item.date).toLocaleDateString()}</div>
-                </div>    
-            })
-            }
+    return (
+        <div className="table-container">
+            {data.length === 0 ? null : (
+                <table className="custom-table">
+                    <thead>
+                        <tr>
+                            <th className="custom-header">Serial Number</th>
+                            <th className="custom-header">Lab Test</th>
+                            <th className="custom-header">Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((item, index) => (
+                            <tr key={item._id}>
+                                <td>{index + 1}</td> {/* Serial Number */}
+                                <td>
+                                    <ul>
+                                        {item.tests.map((element, index) => (
+                                            <li key={index}>{element}</li>
+                                        ))}
+                                    </ul>
+                                </td>
+                                <td>{new Date(item.date).toLocaleDateString()}</td> {/* Date */}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </div>
+
+
     );
 };
 
