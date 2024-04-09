2e51a3b0e4ae06ee01b16583ff69a2a2e15544e6
Sidharth Kumar <sidharthkumar@Sidharths-MacBook-Pro.local>
2024-04-09 09:44:32 +0530

    Added Reminders

--- frontend/src/components/main/Sections/pages/Reminders.jsx
+++ frontend/src/components/main/Sections/pages/Reminders.jsx
@@ -1,12 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import {toast} from 'react-toastify';
+import { toast } from "react-toastify";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const Reminders = () => {
   const [image, setImage] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [medicines,setmedicines] = useState([]);
+  const [medicines, setmedicines] = useState([]);
+  const [medicineDates, setMedicineDates] = useState({});
+  const [userData,setUserData] = useState();
+
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get('http://localhost:3000/users/mydetails');
+      setUserData(() => {
+          return response.data;
+      });
+  };
+  getData();
+  },[]);
 
   const handleImageChange = (e) => {
     setImage(e.target.files[0]);
@@ -17,6 +33,8 @@ const Reminders = () => {
       setError("Please select an image");
       return;
     }
+
+    toast.info("Uploading image please wait");
 
     setIsLoading(true);
     setError(null);
@@ -35,7 +53,7 @@ const Reminders = () => {
         }
       );
       setmedicines(response.data.message);
-      toast.success("Image Uploaded Successfully")
+      toast.success("Image Uploaded Successfully");
       setIsLoading(false);
     } catch (error) {
       setError("Error uploading image");
@@ -43,6 +61,34 @@ const Reminders = () => {
     }
   };
 
+
+  // Reminder timing change
+  const handleDateChange = (date, medicineName) => {
+    setMedicineDates((prevDates) => ({
+      ...prevDates,
+      [medicineName]: date,
+    }));
+    console.log(medicineDates);
+  };
+
+
+  // Reminder submit
+  const handleSetReminder = async () => {
+    const newObject = {
+      medicineDates : [{}],
+      number : userData.phone
+    };
+    Object.entries(medicineDates).map(([medicineName,date]) => {
+      console.log(medicineName,date.toLocaleDateString(),date.toLocaleTimeString());
+      newObject.medicineDates.push({
+        [medicineName] : `${date.toLocaleDateString()}T${date.toLocaleTimeString()}`
+      });
+    });
+    
+    const response = await axios.post('http://localhost:3000/users/reminders', newObject);
+    console.log(response.data); 
+  }
+  
   return (
     <>
       <div className="col-md-12">
@@ -58,10 +104,30 @@ const Reminders = () => {
         {error && <p>Error: {error}</p>}
       </div>
       <div className="col-md-12">
-        {medicines.map((item) => {
-          return <div key={item.medicine}>{item.medicine} - {item.dosage}</div>
+        {medicines.length === 0 ? null : medicines.map((item) => {
+          return (
+            <div key={item.medicine}>
+              <div key={item.medicine}>
+                {item.medicine} - {item.dosage}
+              </div>
+              <DatePicker
+                selected={medicineDates[item.medicine] || new Date()}
+                onChange={(date) => handleDateChange(date, item.medicine)}
+                showTimeSelect
+                dateFormat="Pp"
+              />
+            </div>
+          );
         })}
       </div>
+
+        <div className="col-md-6">
+          <div>Contact Number</div>
+          <input type="number" value={userData ? userData.phone : null} readOnly></input>
+        </div>
+        <div className="col-md-6">
+          <button onClick={handleSetReminder}>Set Reminders</button>
+        </div>
     </>
   );
 };
