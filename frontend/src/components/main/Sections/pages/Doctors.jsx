a491cb9764f13f650576c950898e917779c2394d
Sidharth Kumar <sidharthkumar@Sidharths-MacBook-Pro.local>
2024-04-09 16:19:24 +0530

    Adde doctor data

--- frontend/src/components/main/Sections/pages/Doctors.jsx
+++ frontend/src/components/main/Sections/pages/Doctors.jsx
@@ -1,22 +1,70 @@
-import axios from 'axios';
-import { useEffect } from 'react';
-import {toast} from 'react-toastify';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
-const Doctors =() => {
+import docdata from "../../../../data/doctors_data.json";
+const Doctors = () => {
+  const [data, setData] = useState([]);
+  const [indexes, setIndexes] = useState([]);
 
-    useEffect(() => {
-        const getData = async () => {
-            const response = await axios.get('http://localhost:3000/users/mydetails');
-            console.log(response.data);
-        };
-        getData();
-    }, []);
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get("http://localhost:3000/users/mydetails");
+      console.log(response.data.medInfo.length);
+      setData(() => {
+        return response.data.medInfo;
+      });
 
-    return (
-        <div>
-            Hello world from doctors
-        </div>
-    );
+      setIndexes(() => {
+        let temp = [];
+        for (let i = 0; i < response.data.medInfo.length; i++) {
+          temp.push(getRandomNumber(docdata));
+        }
+        return temp;
+      });
+    };
+    getData();
+  }, []);
+
+  const getRandomNumber = (array) => {
+    const randomNumber = Math.floor(Math.random() * array.length);
+    return randomNumber;
+  };
+
+  return (
+    <div>
+      {data.length === 0
+        ? null
+        : data.map((item) => {
+            return (
+              <div key={item.length}>
+                <div className="col-md-12">{item.medicalHistory[0]}</div>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Doctor Name</th>
+                      <th>Phone</th>
+                      <th>Address</th>
+                      <th>Years of Experience</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {indexes.map((index) => (
+                      <tr key={index}>
+                        <td>{docdata[index].Doctor}</td>
+                        <td>{docdata[index].Phno}</td>
+                        <td>{docdata[index].addr_l}</td>
+                        <td>{docdata[index].year_l}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <br></br>
+              </div>
+            );
+          })}
+    </div>
+  );
 };
 
 export default Doctors;
