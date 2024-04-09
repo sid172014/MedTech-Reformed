1f598b1d361c278e3ad5143bc76bdf9652078c2c
Sidharth Kumar <sidharthkumar@Sidharths-MacBook-Pro.local>
2024-04-09 10:26:25 +0530

    Last

--- frontend/src/components/main/Header/NavAvatar.jsx
+++ frontend/src/components/main/Header/NavAvatar.jsx
@@ -1,16 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from 'axios'
 
 const NavAvatar = () => {
+
+  const [username,setusername] = useState('');
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get('http://localhost:3000/users/mydetails');
+      setusername(() => {
+          return response.data.username;
+      });
+  };
+  getData();
+  },[]);
+
   return (
     <li className="nav-item dropdown pe-3">
       <a className="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
       <img alt="" className="rounded-circle"></img>
-      <span className="d-none d-md-block dropdown-toggle ps-2">F. David</span>
+      <span className="d-none d-md-block dropdown-toggle ps-2">{username.length === 0 ? null : username}</span>
       </a>
 
       <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
         <li className="dropdown-header">
-          <h6>David</h6>
-          <span>Web Developer</span>
+          <h6>{username.length === 0 ? null : username}</h6>
+          <span>Customer</span>
         </li>
 
         <li>
